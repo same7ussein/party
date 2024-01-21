834b2bdf179c93049c145a88c5f0443db177bd45
sameh hussien <riotteam61@gmail.com>
2024-01-21 21:45:29 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+/// <reference types="../@types/jquery" />
+
+$(".openNav").on('click',function () {
+  $("#leftMenu").animate({ width: "250px" }, 50);
+  $("#home-content").animate({ marginLeft: "250px" }, 50);
+});
+
+$(".closebtn").on("click", function () {
+  $("#leftMenu").animate({ width: "0px" }, 50);
+  $("#home-content").animate({ marginLeft: "0px" }, 50);
+});
+
+/*scrollmenu*/
+$("#leftMenu a").on("click", function () {
+  var sectionId = $(this).attr("href");
+
+  var positionOfSection = $(sectionId).offset().top;
+
+  $("html , body").animate({ scrollTop: positionOfSection }, 2000);
+});
+
+/*Accordion*/
+
+$("#sliderDown .toggle").on("click", function () {
+  $(".inner").not($(this).next()).slideUp(500);
+  $(this).next().slideToggle(500);
+});
+
+/*counter*/
+
+window.onload = function () {
+  countDownToTime("25 october 2023 9:00:00");
+};
+
+function countDownToTime(countTo) {
+  let futureDate = new Date(countTo);
+  futureDate = futureDate.getTime() / 1000;
+
+  let now = new Date();
+  now = now.getTime() / 1000;
+
+  timeDifference = futureDate - now;
+
+  let days = Math.floor(timeDifference / (24 * 60 * 60));
+  let hours = Math.floor((timeDifference - days * (24 * 60 * 60)) / 3600);
+  let mins = Math.floor(
+    (timeDifference - days * (24 * 60 * 60) - hours * 3600) / 60
+  );
+  let secs = Math.floor(
+    timeDifference - days * (24 * 60 * 60) - hours * 3600 - mins * 60
+  );
+
+  $(".days").html(`${days} D`);
+  $(".hours").html(`${hours} h`);
+  $(".minutes").html(`${mins} m`);
+  $(".seconds").html(`${secs} s`);
+
+  setInterval(function () {
+    countDownToTime(countTo);
+  }, 1000);
+}
+
+/*textarea*/
+
+var maxLength = 100;
+$("textarea").on('keyup',function () {
+  var length = $(this).val().length;
+  var AmountLeft = maxLength - length;
+  if (AmountLeft <= 0) {
+    $("#chars").text("your available character finished");
+  } else {
+    $("#chars").text(AmountLeft);
+  }
+});
